83a4ed8c627aa484ec7827e0f301bed6dbd939e6
Chris Arderne <chris@rdrn.me>
2024-07-31 22:51:33 +0100

    vendor sqlcipher and build pysqlcipher3 amalgamation (#135)
    * move code into src
    
    * matrixify py version
    
    * add sqlcipher build script
    
    * add build script
    
    * vendor sqlcipher
    
    * only one matrix build
    
    * without cibuild
    
    * fix rename
    
    * fix upload path
    
    * multi arch
    
    * add setuptools
    
    * failfast=false
    
    * multi linux python
    
    * single py version
    
    * try again for mac
    
    * cd
    
    * remove Dockerfile
    
    * pytest
    
    * single cicd
    
    * fix
    
    * try this
    
    * try this
    
    * rename macos wheel
    
    * add sdist
    
    * add sqlcipher readme
    
    * rename

--- sigexport/__main__.py
+++
@@ -1,4 +0,0 @@
-from sigexport.main import cli
-
-if __name__ == "__main__":
-    cli()
--- sigexport/logging.py
+++
@@ -1,8 +0,0 @@
-from typer import colors, secho
-
-verbose = False
-
-
-def log(msg: str, fg: str = colors.BLACK) -> None:
-    if verbose:
-        secho(msg, fg=fg)
